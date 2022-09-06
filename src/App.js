30878ccfd74fbb65c0675594de847616d679c75a
George Ciurescu <george.ciurescu@verint.com>
2022-09-06 17:26:42 +0100

    Adding more pages

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
-import FeedbackData from "./data/FeedbackData";
-import React from "react";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
-import {v4 as uuidv4} from 'uuid'
+import AboutIconLink from "./components/AboutIconLink";
+import AboutPage from "./pages/AboutPage";
+import { FeedbackProvider } from "./context/FeedbackContext";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback,...feedback])
-  };
-
-  const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure you want to delet???")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
-    }
-  };
-
   return (
-    <>
-      <Header />
-      <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
-    </>
-  );
-}
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={
+              <>
+                <FeedbackForm />
+                <FeedbackStats />
+                <FeedbackList />
+              </>
+            }>
+            </Route>
+
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+
+          <AboutIconLink />
+        </div>
+      </Router >
+    </FeedbackProvider>
+  )
+};
 
 export default App;
